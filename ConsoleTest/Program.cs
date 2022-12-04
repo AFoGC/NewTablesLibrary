b0cffc4fa87ffe67de934a3113837ca7a436a3ea
AFoGC <sirkoandrew84@gmail.com>
2022-12-04 02:26:57 +0200

    Дописал класс Command и Добавил два проекта для тестирования

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NewTablesLibrary;
+
+Command command = new Command();
+command.GetCommand("    <name: Simple> ///");
+Console.WriteLine(command.FieldName + "|");
+Console.WriteLine(command.FieldValue + "|");
--- NewTablesLibrary/Command.cs
+++ NewTablesLibrary/Command.cs
@@ -23,25 +23,61 @@ namespace NewTablesLibrary
         public bool IsCommand => _isCommand;
         public bool IsMark => _isMark;
 
-        private void SetCommand(string fieldName, string fieldValue)
+        public bool GetCommand(string line)
+        {
+            string fieldName = string.Empty;
+            string fieldValue = string.Empty;
+            int startOfCommand = line.IndexOf('<');
+            int endOfCommmand = line.IndexOf('>');
+            int separatorInxex = line.IndexOf(':');
+
+            if (startOfCommand == -1 || endOfCommmand == -1)
+            {
+                SetWrongCommand();
+                return false;
+            }
+
+            startOfCommand += 1;
+
+            if (separatorInxex == -1)
+            {
+                fieldName = line.Substring(startOfCommand, endOfCommmand - startOfCommand);
+                SetMark(fieldName);
+            }
+            else
+            {
+                fieldName = line.Substring(startOfCommand, separatorInxex - startOfCommand);
+
+                separatorInxex += 2;
+                fieldValue = line.Substring(separatorInxex, endOfCommmand - separatorInxex);
+                SetFullCommand(fieldName, fieldValue);
+            }
+
+            return true;
+        }
+
+        private void SetFullCommand(string fieldName, string fieldValue)
         {
             _fieldName = fieldName;
             _fieldValue = fieldValue;
+            _isMark = false;
             _isCommand = true;
-
-            if (_fieldValue == string.Empty)
-                _isMark = true;
-
-            if (_fieldName == string.Empty && _fieldValue == string.Empty)
-            {
-                _isCommand = false;
-                _isMark = false;
-            }
         }
 
-        public void GetCommand()
+        private void SetMark(string fieldName)
         {
+            _fieldName = fieldName;
+            _fieldValue = string.Empty;
+            _isMark = true;
+            _isCommand = true;
+        }
 
+        private void SetWrongCommand()
+        {
+            _fieldName = string.Empty;
+            _fieldValue = string.Empty;
+            _isCommand = false;
+            _isMark = false;
         }
     }
 }
