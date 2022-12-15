5b7e660cba386a1b4cabdf7c633011663f1371e0
AFoGC <sirkoandrew84@gmail.com>
2022-12-15 03:31:17 +0200

    Перенёс все проекты на .NET, а также перенёс тесты на XUnit

---
+++ NewTablesLibrary.Tests/CommandTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace NewTablesLibrary.Tests
+{
+    public class CommandTests
+    {
+        [Fact]
+        public void TestDefaultCommand()
+        {
+            Command command = new Command();
+            bool isCommand = command.GetCommand("    <name: Simple> ///");
+
+            Assert.Equal("name", command.FieldName);
+            Assert.Equal("Simple", command.FieldValue);
+            Assert.True(command.IsCommand);
+            Assert.True(isCommand);
+        }
+
+        [Fact]
+        public void TestMarkCommand()
+        {
+            Command command = new Command();
+            bool isCommand = command.GetCommand("<Marka>");
+
+            Assert.Equal("Marka", command.FieldName);
+            Assert.True(command.IsMark);
+            Assert.True(command.IsCommand);
+            Assert.True(isCommand);
+        }
+
+        [Fact]
+        public void TestWrongCommand()
+        {
+            Command command = new Command();
+            bool isCommand = command.GetCommand("Ada: fa");
+
+            Assert.False(isCommand);
+            Assert.False(command.IsCommand);
+        }
+    }
+}
